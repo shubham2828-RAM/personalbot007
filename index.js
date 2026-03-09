eb01926ac4ec4d76fb92fcc4b2886d80f7463c34
ShubhMac <shubhamji4646@gmail.com>
2026-03-09 23:10:47 +0530

    09-03-2026

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const shortid = require('shortid');
 const client = new Client({ intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent] });
 
 const mongoose = require('mongoose');
+const shortVal = shortid.generate();
 
 mongoose.connect('mongodb://localhost:27017/testdb').then(() => {
     console.log('Connected to MongoDB');
@@ -13,7 +15,9 @@ mongoose.connect('mongodb://localhost:27017/testdb').then(() => {
 });
 
 const userSchema = require('./schema').user;
+const urlShortId = require('./schema').urlShortId;
 const tokenAuth = mongoose.model('token', userSchema); 
+const urlShortIdMod = mongoose.model('urlShortId', urlShortId);
 
 tokenAuth.findOne({}).then((result) => {
     if (result) {
@@ -26,6 +30,20 @@ tokenAuth.findOne({}).then((result) => {
 
 client.on('messageCreate', message => {
     if (message.author.bot) return;
+    if (message.content.startsWith('create')) {
+        const url = message.content.split('create ')[1];
+        urlShortIdMod.create({ originalUrl: url, shortId: shortVal }).then(() => {
+            console.log('URL shortened and saved to database');
+        }).catch((err) => {
+            console.error('Error saving URL to database:', err);
+        });
+        message.reply({
+            content: `Generating shorturl for : ${url}`
+        })
+        return message.reply({
+            content : `Short URL for ${url} is: http://short.url/${shortVal}`
+        });
+    }
     message.reply({
         content: 'Hello, I am a bot!'
     });
